c4b18d43895ca007214042e04aa5be4a11b4b352
adhfarmujtaba <adhfarrather@gmail.com>
2024-10-08 18:35:03 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,3 @@
 const nextConfig = {};
 
 export default nextConfig;
-// next.config.js
-module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/[category_slug]/[slug]', // Replace with your SSG route
-          destination: '/ssr/[category_slug]/[slug]', // Redirect to the SSR route
-          permanent: false, // Set to true if this is a permanent redirect (301)
-        },
-      ];
-    },
-  };
-  
