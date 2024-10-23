1eac26f74d48bd646b708104202f418f1f097a13
adhfarmujtaba <adhfarrather@gmail.com>
2024-10-23 22:15:05 +0530

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -86,6 +86,9 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       <SideMenu isMenuOpen={menuOpen} handleToggleMenu={toggleMenu} />
 
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Pacifico&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
